699073527384f850c0ad90556b92427405fefae9
MARO <MARO@MM-TP-MARO.machcorp.lan>
2013-10-13 19:38:03 +0200

    - first version with a menubar

--- src/main/java/komodo/rpg/sounds/RPGSounds.java
+++ src/main/java/komodo/rpg/sounds/RPGSounds.java
@@ -1,6 +1,10 @@
 package komodo.rpg.sounds;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
 import javax.swing.JTree;
 
 public class RPGSounds extends JFrame {
@@ -8,6 +12,24 @@ public class RPGSounds extends JFrame {
 	private JTree clipTree;
 	
 	public RPGSounds() {
+		super("RPG Sounds");
+		
+		JMenuBar menubar = new JMenuBar();
+		
+		JMenu file = new JMenu("Datei");
+		menubar.add(file);
+		JMenuItem menu_new = new JMenuItem("Neu...");
+		file.add(menu_new);
+		JMenuItem menu_open = new JMenuItem("Open...");
+		file.add(menu_open);
+		file.add(new JSeparator());
+		JMenuItem menu_exit = new JMenuItem("Beenden");
+		file.add(menu_exit);
+		
+		this.setJMenuBar(menubar);
+		
+		
+		
 		setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
