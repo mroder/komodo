acbcca0eff8db4a3b4c76057f727babb3b740e4e
Markus Roder <maro@mach.com>
2013-10-21 17:18:25 +0200

    - experiments extending the TabbedPane

--- src/main/java/komodo/rpg/sounds/ProjectPane.java
+++ src/main/java/komodo/rpg/sounds/ProjectPane.java
@@ -1,27 +1,27 @@
 package komodo.rpg.sounds;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSlider;
 import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import komodo.rpg.sounds.ui.ClipPlayerPanel;
 
-public class ProjectPane extends JPanel {
+public class ProjectPane extends JPanel implements ChangeListener {
 
 	private JTabbedPane tabPane;
 	
 	public ProjectPane() {
 		setLayout(new BorderLayout());
 		
+		
 		tabPane = new JTabbedPane();
+		tabPane.addChangeListener(this);
 		add(tabPane,BorderLayout.CENTER);
-		tabPane.addTab("Test 1", new JLabel("First Tab"));
-		tabPane.addTab("Test 2", new JLabel("Second Tab"));
 		JPanel i = new JPanel();
 		i.setOpaque(false);
 //		i.setLayout(new FlowLayout());
@@ -30,6 +30,22 @@ public class ProjectPane extends JPanel {
 		i.add(new ClipPlayerPanel(null));
 		tabPane.addTab("Test 3", i);
 		
+		
 		add(new JLabel("<html><h3>Action Buttons</h3></html>"), BorderLayout.SOUTH);
+		
+		addScene();
+	}
+	
+	public void addScene() {
+		tabPane.addTab("test", new ImageIcon(getClass().getResource("/imgs/icon_play.png")), new JLabel());
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		System.out.println(e.toString());
+		System.out.println(tabPane.getTitleAt(tabPane.getSelectedIndex()));
+		System.out.println(tabPane.getSelectedIndex());
+		System.out.println(tabPane.getComponent(tabPane.getSelectedIndex()));
+		
 	}
 }
